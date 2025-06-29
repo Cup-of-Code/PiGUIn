558b870f1e3b293ceaad21607cbd59394a924494
Charlotta Larmoijeur <102232378+Cup-of-Code@users.noreply.github.com>
2025-06-29 23:58:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import sys
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QMenu, QLabel, QVBoxLayout,QWidget
+
+from PyQt5.QtCore import (
+    QSize,
+    Qt,
+    QTimer, QTime,
+    )
+
+from PyQt5.QtWidgets import ( 
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QMenu,
+    QLabel,
+    QGridLayout,
+    QWidget,
+    QToolBar,
+
+    )
 
 
 class menuButton(QPushButton):
@@ -11,24 +27,38 @@ class menuButton(QPushButton):
         super().__init__(title)
         self.title = title
         self.setFixedSize(width, height)
-        self.setStyleSheet("background-color: #093; color: black; border-radius: 15px; font-size: 30px;")
+        self.setStyleSheet("background-color: #093; color: black; border-radius: 15px;font-family: 'Daily Bubble'; font-size: 45px;")
         self.clicked.connect(self.buttonClick)
         
     def buttonClick(self): #funktionalitet för att se om knappen har klickats
         print("knappen: "+ self.title + " klickades.")
         
         
+
+
 class timeKeeper(QLabel):
     """
     En klass som skapar en "label" som sedan visar den aktuella tiden på startsidan.
     """
-   
-    def __init__(self, title, width= 667, height=50):
-        super().__init__(title)
-        self.setFixedSize(width, height)
-        self.setStyleSheet("color: black; font-size: 48px;")
-        
-    #Kod för att hämta och visa tiden kommer läggas till nedan.
+    def __init__(self, width=667, height=90):
+        super().__init__()
+        self.setFixedSize(int(width), int(height))  # ensure width and height are integers
+        self.setStyleSheet("color: black; font-family: 'Super Vibes'; font-size: 60px;") #Super Vibes är en custom font:  dafont.com/super-vibes.font
+
+
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.updateClock)
+        self.timer.start(1000)
+
+        self.updateClock()
+
+    def updateClock(self):
+        current_time = QTime.currentTime().toString('hh:mm:ss')
+        self.setText(current_time)
+
+
+            
 
 
 
@@ -40,18 +70,60 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setFixedSize(QSize(667, 1000)) #667x1000 är samma ratio som LCD-skärmen (320x480)
         self.setWindowTitle("PiGUIn")
-        layout = QVBoxLayout() #med denna layout så läggs de definierade "widget"-sen som på en vertikal stapel.
+        layout = QGridLayout() #med denna layout så sorteras de tillagda komponenterna i en grid-layout
         
-        appButton = menuButton("app1")
-        currentTime = timeKeeper("Time now:      XX : XX")
+        statsButton = menuButton("System stats")
+        statsButton.clicked.connect(self.showstats) #byter till systemstatsfönstret när knappen klickas
         
-        layout.addWidget(currentTime)
-        layout.addWidget(appButton)
+        fileButton = menuButton("Files")
+        currentTime = timeKeeper()
+        
+        layout.addWidget(currentTime,0,0) #0,1 indikerar rad 0, kolumn 1 i grid-layouten
+        layout.addWidget(statsButton, 1,0)
+        layout.addWidget(fileButton,1,1)
 
         containerBox = QWidget()
         containerBox.setLayout(layout)
         self.setCentralWidget(containerBox)
+        self.statsPage = statsWindow()
         
+        
+        
+    def showstats(self, checked):
+       self.statsWindow = statsWindow()
+       self.statsWindow.show()
+        
+        
+        
+        
+class statsWindow(QWidget):
+    """
+        En klass för systemstatistik fönstret
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.system_temp = self.systemTemp()
+        sysTemp = self.systemTemp()
+        tempLabel = QLabel("System temp: "+ str(sysTemp))
+        self.setStyleSheet("color: black; font-family: 'Daily Bubble'; font-size: 40px;")
+        layout = QGridLayout()
+        layout.addWidget(tempLabel)
+        self.setLayout(layout)
+        
+        
+        
+    def systemTemp(self):
+         try:
+             with open ('/sys/class/thermal/thermal_zone0/temp') as tempStatFile: #systemStat path här är definierad för mitt specifika OS.
+                 tempValue = tempStatFile.read().strip()
+                 return float(tempValue) / 1000.0
+         except:
+             print("could not locate temp info")
+             return None
+         
+    
+    
 
 app = QApplication(sys.argv)
 
